e3f884b5be55f9942eca220fc501bec025c72510
LVQA1998 <quanganlevan@gmail.com>
2020-04-20 10:29:30 +0700

    update static pages

--- test/controllers/static_page_controller_test.rb
+++ test/controllers/static_page_controller_test.rb
@@ -1,14 +1,27 @@
 require 'test_helper'
 
 class StaticPageControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @base_name = "Ruby on rails Tutorial Sample App"
+  end
+
   test "should get home" do
     get static_page_home_url
     assert_response :success
+    assert_select "title", "Home | #{@base_name}" 
+
   end
 
   test "should get help" do
     get static_page_help_url
     assert_response :success
+    assert_select "title", "Help | #{@base_name}" 
+
   end
 
+  test "should get about" do 
+    get static_page_about_url
+    assert_response:success
+    assert_select "title", "About | #{@base_name}" 
+  end
 end
